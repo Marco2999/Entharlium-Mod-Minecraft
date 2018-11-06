044a2f48610420db27d5dcc087f1cce901c0b7c3
Francisco Saltiel <francisco.saltiel@upr.edu>
2018-11-05 23:36:23 -0400

    added block properties to entharlium furnace

---
+++ src/main/java/blocks/EntharliumFurnace.java
@@ -0,0 +1,18 @@
+package blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class EntharliumFurnace extends BlockBase {
+
+	public EntharliumFurnace(String name, Material material) {
+		super(name, material);
+		
+		setSoundType(SoundType.STONE);
+		setHardness(3.5f);
+		setResistance(17.5f);
+		setHarvestLevel("pickaxe", 1);
+		setLightLevel(13.0f);
+	}
+	
+}
